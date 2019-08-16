3188f38d53f8c05a0e1ae2febae2f2956a73af8b
Ivan S Glazunov <ivansglazunov@gmail.com>
2019-08-16 16:27:00 +0300

    profmoda1 images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,72 @@
-import { useGql } from '../imports/use-gql';
-import { GET_NODES } from '../imports/sandbox';
+import { Parallax } from "react-parallax";
+
+import '../style.css';
+
+export const WhiteSpace = () => {
+  return <div style={{ height: 500 }}></div>;
+};
 
 export default () => {
-  const query = useGql(GET_NODES);
-  return <pre>{JSON.stringify(query, null, 2)}</pre>;
+  return <>
+    <Parallax
+      bgImage={'/static/1.jpg'}
+      strength={200}
+      renderLayer={percentage => (
+        <div>
+          <div
+            style={{
+              position: "absolute",
+              background: `rgba(255, 125, 0, ${percentage * 1})`,
+              left: "50%",
+              top: "50%",
+              borderRadius: "50%",
+              transform: "translate(-50%,-50%)",
+              width: percentage * 500,
+              height: percentage * 500
+            }}
+          />
+        </div>
+      )}
+    >
+      <div style={{ height: 500 }}>
+      </div>
+    </Parallax>
+    <Parallax
+      bgImage={'/static/0.jpg'}
+      strength={300}
+    >
+      <div style={{ height: 500 }}>
+      </div>
+    </Parallax>
+    <Parallax
+      bgImage={'/static/0.jpg'}
+      strength={300}
+    >
+      <div style={{ height: 500 }}>
+      </div>
+    </Parallax>
+    <Parallax
+      bgImage={'/static/1.jpg'}
+      strength={200}
+      renderLayer={percentage => (
+        <div>
+          <div
+            style={{
+              position: "absolute",
+              background: `rgba(255, 125, 0, ${percentage * 1})`,
+              left: "50%",
+              top: "50%",
+              borderRadius: "50%",
+              transform: "translate(-50%,-50%)",
+              width: percentage * 500,
+              height: percentage * 500
+            }}
+          />
+        </div>
+      )}
+    >
+      <div style={{ height: 500 }}>
+      </div>
+    </Parallax>
+  </>;
 };
